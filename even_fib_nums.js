08b229109edbbe25347a4ae6be5555c4794d0cfc
kmietotwy <kmietotwy@gmail.com>
2019-04-02 18:21:51 -1000

    fib exercises

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -9,7 +9,18 @@ function _sumFibs( maxFibValue ) {
   var sum = 0;
 
   // do your work here
+  let firstFib = 0;
+  let secondFib = 1;
+  let thirdFib = 0;
 
+  while(thirdFib <= maxFibValue){
+    thirdFib = firstFib + secondFib;
+    if(thirdFib%2 === 0){
+      sum+=thirdFib;
+    }
+    firstFib = secondFib;
+    secondFib = thirdFib;
+  }
   return sum;
 }
 
@@ -18,12 +29,40 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
-
+  let firstFib = 0;
+  let secondFib = 1;
+  let thirdFib = 0;
 
   //do your work here
-
+  while(thirdFib <= maxFibValue){
+    thirdFib = firstFib + secondFib;
+    firstFib = secondFib;
+    secondFib = thirdFib;
+  }
+  highest = firstFib;
   return highest;
 };
+
+//stretch goals
+function _sumFibs2( maxFibValue ) {
+  var sum = 0;
+
+  // do your work here
+  let firstFib = 0;
+  let secondFib = 1;
+  let thirdFib = 0;
+
+  while(thirdFib <= maxFibValue){
+    thirdFib = firstFib + secondFib;
+    if(thirdFib%2 === 0 || thirdFib%5 === 0){
+      sum+=thirdFib;
+    }
+    firstFib = secondFib;
+    secondFib = thirdFib;
+  }
+  return sum - secondFib;
+}
+
 
 /**
  * Do not modify code below.
@@ -31,5 +70,6 @@ function _highestFibonacciNumber (maxFibValue){
  */
 module.exports = {
   sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  highestFibonacciNumber : _highestFibonacciNumber,
+  sumFibs2 : _sumFibs2
 };
